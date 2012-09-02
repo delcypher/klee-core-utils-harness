e45178ceb2d6ff8ff87cc9a672f4705f582eb019
Dan Liew <delcypher@gmail.com>
2012-09-02 12:01:22 +0100

    Improved script so that special KLEE parameters needed for some core-utils are passed.

--- klee-cu-harness.sh
+++ klee-cu-harness.sh
@@ -6,7 +6,7 @@
 #Default sym args
 defaultArgs='--sym-args 0 1 10 --sym-args 0 2 2 --sym-files 1 8 --sym-stdout'
 
-RUN_IN="/home/dan/sandbox"
+RUN_IN="/data/dsl11/sandbox"
 
 #Allow more open files than the default
 ulimit -n 4096
@@ -14,7 +14,8 @@ ulimit -n 4096
 #setup trap so that C^C causes loop to exit
 trap exit SIGINT SIGTERM SIGQUIT
 
-#List of core-utils (key) to test with sym args. This is identical to the list originally used in the KLEE paper.
+#List of core-utils (key) to test with sym args. This is similar to the list originally used in the KLEE paper.
+#If a utility does not have specified sym args then "defaultArgs" are used.
 declare -A UTILS
 UTILS=( ["["]='--sym-args 0 1 10 --sym-args 0 4 3 --sym-files 2 10 --sym-stdout' \
 	[base64]= \
@@ -107,7 +108,15 @@ UTILS=( ["["]='--sym-args 0 1 10 --sym-args 0 4 3 --sym-files 2 10 --sym-stdout'
 	[yes]= \
 )
 
+#List of special KLEE options to use with utilities
+#If a utility is not in this array then no extra options are passed to KLEE
+declare -A EXTRA_ARGS
+EXTRA_ARGS=( [md5sum]='--max-solver-time=120' \
+	     [sort]='--max-sym-array-size=1024' \
+)
+
 ARG=""
+SPECIAL_ARG=""
 
 if [ ! -d "${RUN_IN}" ]; then
 	echo "Run in directory : ${RUN_IN} does not exist!"
@@ -125,6 +134,7 @@ do
 		UTIL_BC="${util}.bc"
 		if [ ! -r "${UTIL_BC}" ]; then
 			echo "Utility ${UTIL_BC} not found!"
+			continue;
 		fi
 		
 		#determine sym args to use
@@ -135,12 +145,21 @@ do
 			echo "$util is using non default $ARG"
 		fi
 
+		#Determine if we need to use extra arguments to KLEE
+		if [ -z "${EXTRA_ARGS["$util"]}" ]; then
+			SPECIAL_ARG="";
+		else
+			SPECIAL_ARG="${EXTRA_ARGS["$util"]}"
+			echo "${util} has extra KLEE arguments: ${SPECIAL_ARG}"
+
+		fi
+
 		OUTPUT_DIR="klee-${util}-${run}"
 
 		echo "*****Running '${OUTPUT_DIR}' at $(date)"
 
 		#Now run klee
-		#NOTE if running sort will need to use --max-sym-array-size=1024
+		#Remove appropriate lines depending on which solver you want to use
 		klee \
 		--solver=stp \
 #		--solver=smtlibv2 \
@@ -154,6 +173,7 @@ do
 		--max-time=1200 \
 		--watchdog \
 		--run-in="${RUN_IN}" \
+		${SPECIAL_ARG} \
 		${UTIL_BC} \
 		${ARG} \
 		1> /dev/null \
